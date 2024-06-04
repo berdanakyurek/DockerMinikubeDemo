1f3842feae7ff8488faf7895ee87b87b89f7d4a7
Berdan Akyurek <berdan@teus.com.tr>
2024-06-04 14:12:52 +0300

    init

---
+++ Backend/Backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "HelloWorld")]
+        public String Get()
+        {
+            return "Bu Mesaj API'den Gelmektedir.";
+        }
+    }
+}
